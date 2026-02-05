486058fe4aa3f38633881fbcb55f9a82fe860a40
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-02-05 10:38:25 -0800

    Added Scaling to obsticals and lane reset.

--- src/prefabs/Obstical.js
+++ src/prefabs/Obstical.js
@@ -5,13 +5,29 @@ class Obstical extends Phaser.GameObjects.Image
         super(scene,x,y,texture,frame)
         scene.add.existing(this)
         this.moveSpeed = speed 
+        this.setScale(0.1,0.1)
     }
 
-    update()
+        //scale the object up to a set size and move it down the screen, so it looks like it's coming toward the player
+    moveObstical()
     {
-  //  this.enemy1.displayWidth += this.moveSpeed
-  //  this.enemy1.displayHeight += this.moveSpeed
+        this.y += this.moveSpeed
+        if(this.scaleX < 3){
+        console.log(this.scaleX)
+        this.setScale(this.scaleX + 0.1, this.scaleY + 0.01)
+        }
+
+
     }
+
+    respawnObstical(lanePostion,speed)
+        {
+            this.y = 0
+            this.x = lanePostion
+            this.setScale(0.1,0.1)
+            this.moveSpeed = speed
+        }
+    
 
 
 }
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -1,27 +1,64 @@
-class Play extends Phaser.Scene
-{
-    constructor()
-    {
+class Play extends Phaser.Scene {
+    constructor() {
         super("playScene")
     }
 
-    create()
-    {
+    create() {
+        this.speed = 20
+        this.LanePostions = [(width / 3) / 2,(width / 3) + (width / 3) / 2,(width / 3) * 2 + (width / 3) / 2]
         //Make lane lines
-        this.add.rectangle(width /3,height/2,10,height,0x0000f)
-        this.add.rectangle((width/3) *2,height/2,10,height,0x0000f)
-        
+        this.add.rectangle(width / 3, height / 2, 10, height, 0x0000f)
+        this.add.rectangle((width / 3) * 2, height / 2, 10, height, 0x0000f)
+
 
         //adding enemies to lanes to test positioning (should have a factory that produces these)
-        this.enemy1 = new Obstical(this,(width/3)/2,50,'brick',0,20)
-        this.enemy2 = new Obstical(this,(width/3)+(width/3)/2,50,'brick',0,20)
-        this.enemy3 = new Obstical(this,(width/3)*2 +(width/3)/2,50,'brick',0,20)
+        this.enemy1 = new Obstical(this, (width / 3) / 2, 50, 'brick', 0, this.speed)
+        this.enemy2 = new Obstical(this, (width / 3) + (width / 3) / 2, 50, 'brick', 0, this.speed)
+        this.enemy3 = new Obstical(this, (width / 3) * 2 + (width / 3) / 2, 50, 'brick', 0, this.speed)
 
         //add player
-        this.player = new Player(this,'player')
+        this.player = new Player(this, 'player')
     }
 
-    update()
-    {
+    update() {
+        this.updateLanePosition()
+        this.enemy1.moveObstical()
+        this.enemy2.moveObstical()
+        this.enemy3.moveObstical()
+
+        
+
+        if(this.enemy1.y > height + 100)
+        {
+            this.enemy1.respawnObstical(this.LanePostions[Phaser.Math.Between(0,2)],Phaser.Math.Between(this.speed-10,this.speed))
+        }
+         if(this.enemy2.y > height + 100)
+        {
+            this.enemy2.respawnObstical(this.LanePostions[Phaser.Math.Between(0,2)],Phaser.Math.Between(this.speed-10,this.speed))
+        }
+         if(this.enemy3.y > height + 100)
+        {
+            this.enemy3.respawnObstical(this.LanePostions[Phaser.Math.Between(0,2)],Phaser.Math.Between(this.speed-10,this.speed))
+        }
+        //Speeds up the enemy over time, needs to be reworked, I dont like handling this here.
+        this.speed+= .001
+    }
+
+    //Might try to migrate this to Player.js , I feel this does not need to be checked everyframe, just when lanepos changes
+    updateLanePosition() {
+        switch (this.player.lanePos) {
+            case 1:
+                console.log("case 1")
+                this.player.setX((width / 3) / 2)
+                break
+            case 2:
+                console.log("case 2")
+                this.player.setX((width / 3) + (width / 3) / 2)
+                break
+            case 3:
+                console.log("case 3")
+                this.player.setX((width / 3) * 2 + (width / 3) / 2)
+                break
+        }
     }
 }
