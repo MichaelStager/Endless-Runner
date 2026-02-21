27f679fa3efaca0d0eb405d360ac705ead222a5d
Michael Stager <61891711+MichaelStager@users.noreply.github.com>
2026-02-20 20:39:22 -0800

    GG

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 //Michael Stager
 //2/3/2026
 //HELLTER SKELLTER
-// For my coding technically interesting topic I couldnt think off anything else besides the example altice did, with a highscore that saves to local storage.
-// For the Artistic intresting topic it should be clear. The Game revolves around layered asetics stacking effects. I Want it to show chaos and resembles that of jumpstyle art
-// The game itself has a underlying tone of testing faith, you are tasked with making it to the top of the tower, something you can never reach, resembing the inability to make it to heaven, and thats why the game gets scarier and scarier the more you progress 
+// For my coding technically interesting topic, I couldn’t think of anything else besides the example Altice did: a high score that saves to local storage.
+// For the artistic interesting topic, it should be clear. The game revolves around layered aesthetics stacking effects. I want it to show chaos and resemble jumpstyle art.
+// The game itself has an underlying tone of testing faith: you are tasked with making it to the top of the tower,something you can never reach,resembling the inability to make it to heaven. That’s why the game gets scarier and scarier the more you progress.
+// This is also the reason for the Leviathan cross on the shield: this symbol represents self-guidance and self-fulfillment. There are many little details like that, but I will spare you the time.
 
 
 
